83e6e8e9bbe5e2f0dddcf88e2973225d4d357ef0
Tanner Nelson <tanner@bluebite.com>
2016-04-07 22:13:47 -0400

    update to 0.5

--- App/main.swift
+++ App/main.swift
@@ -7,12 +7,9 @@ app.get("/") { request in
 	return try app.view("home.mustache")
 }
 
-//Add includeable files to the Mustache provider
-VaporZewoMustache.Provider.includeFiles = [
-    "header": "Includes/header.mustache",
+app.providers.append(VaporZewoMustache.Provider(withIncludes:[
+	"header": "Includes/header.mustache",
     "footer": "Includes/footer.mustache"
-]
-
-app.providers.append(VaporZewoMustache.Provider)
+]))
 
 app.start(port: 8080)
