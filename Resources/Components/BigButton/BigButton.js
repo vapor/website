325838dfc059beaf5e958c0a77a3345800115bee
Tanner Nelson <tanner@bluebite.com>
2016-04-12 19:12:34 -0400

    change install link for docs

--- Resources/Components/BigButton/BigButton.js
+++ Resources/Components/BigButton/BigButton.js
@@ -8,7 +8,7 @@ export default {
 			this.$root.log('[Big Button] ' + message)
 		},
 		click() {
-			window.location.href = 'https://github.com/qutheory/vapor/wiki/Setup';
+			window.location.href = 'http://docs.qutheory.io';
 		}
 	},
 	ready() {
