0d1a00e9fce2852de082c9549c8a5e4cbf93d065
Nathan Flurry <bitbucket@nathanflurry.com>
2016-05-27 12:43:32 -0700

    Added Twitter link to navigation
    - Added Twitter link to navigation bar
    - Removed unnecessary UserController.swift and User.swift files
    - Updated to Vapor 0.9 and switched from VaporZewoMustache to VaporMustache
    - Updated to DEVELOPMENT-SNAPSHOT-2016-05-09-a

--- App/main.swift
+++ App/main.swift
@@ -1,5 +1,5 @@
 import Vapor
-import VaporZewoMustache
+import VaporMustache
 
 let app = Application()
 
@@ -7,9 +7,9 @@ app.get("/") { request in
 	return try app.view("home.mustache")
 }
 
-app.providers.append(VaporZewoMustache.Provider(withIncludes:[
+app.providers.append(VaporMustache.Provider(withIncludes:[
 	"header": "Includes/header.mustache",
     "footer": "Includes/footer.mustache"
 ]))
 
-app.start(port: 8080)
+app.start()
