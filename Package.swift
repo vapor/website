69022a96face2cfe9a8446187686afc5eabd177d
Tanner Nelson <me@tanner.xyz>
2016-06-28 16:14:32 -0400

    update mustache

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "VaporWebsite",
     dependencies: [
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 0) //will be 0.8
+        .Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 8)
     ],
     exclude: [
         "Deploy",
