e19d36ba3f8225b3c79b424c9251259478df3af2
alxstu <alx.stu@gmail.com>
2018-05-04 23:43:06 +0200

    added target="_blank" to external links
    this opens the external links in a new tab/window

--- src/app.vue
+++ src/app.vue
@@ -6,8 +6,8 @@
         <div class="dark">
             <div class="wrapper">
                 <div class="bios">
-                    <vapor-bio image="rocket">Non-blocking, event-driven architecture built on top of Apple's <a href="https://github.com/apple/swift-nio">SwiftNIO</a> delivers <a href="#">high performance</a>.</vapor-bio>
-                    <vapor-bio image="swift">Written in <a href="https://swift.org">Swift</a>, the powerful programming language that is also easy to learn.</vapor-bio>
+                    <vapor-bio image="rocket">Non-blocking, event-driven architecture built on top of Apple's <a target="_blank" href="https://github.com/apple/swift-nio">SwiftNIO</a> delivers <a href="#">high performance</a>.</vapor-bio>
+                    <vapor-bio image="swift">Written in <a target="_blank" href="https://swift.org">Swift</a>, the powerful programming language that is also easy to learn.</vapor-bio>
                     <vapor-bio image="pacman">Expressive, <a target="_blank" href="https://developer.apple.com/videos/play/wwdc2015/408/">protocol-oriented design</a> with a focus on type-safety and maintainability.</vapor-bio>
                 </div>
                 <div class="packages">
