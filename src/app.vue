0067b43523d185898d0b2320e2dba4a22bf60453
Nathan Harris <2nd.lt.harris@gmail.com>
2018-05-05 22:45:06 -0700

    Update Footer
    - Add: Footer component to keep data self-contained
    - Change: App styling to use SCSS syntax over SASS
    - Remove: Old styling in App

--- src/app.vue
+++ src/app.vue
@@ -16,161 +16,37 @@
             <vapor-contribute class="vapor-app__contribute"/>
 
             <vapor-sponsor class="vapor-app__sponsor"/>
-
-
-            <div class="vapor-footer">
-                <div class="footer-nav">
-                    <a href="mailto:hello@vapor.codes">Contact Us</a>
-                    <a target="_blank" href="https://twitter.com/codevapor">Twitter</a>
-                    <a target="_blank" href="https://medium.com/@codevapor">Medium</a>
-                    <a href="/vapor-presskit-2018-05-04.zip">Press Kit</a>
-                    <a href="mailto:jobs@vapor.codes">Jobs</a>
-                </div>
-                <p>&copy; Qutheory, LLC 2018</p>
-            </div>
-            <!--             <v-footer app>
-                            <span>&copy; Qutheory, LLC 2018</span>
-                        </v-footer> -->
         </main>
+
+        <vapor-footer class="vapor-app__footer"/>
     </div>
 </template>
 
-<style lang="sass">
-    .vapor-app
-        font-family: 'Roboto', sans-serif
+<style lang="scss">
+    .vapor-app {
+        font-family: 'Roboto', sans-serif;
 
-    .vapor-app__content
-        display: flex
-        width: 100%;
-        flex-direction: column
-        align-items: center
+        .vapor-app__content {
+            display: flex;
+            width: 100%;
+            flex-direction: column;
+            align-items: center;
 
-        > *:not(:first-child)
-                margin-top: 1em
+            > *:not(:first-child) {
+                margin-top: 1em;
+            }
 
-        .vapor-app__packages, .vapor-app__call-to-action
-                margin-top: 0
+            .vapor-app__packages, .vapor-app__call-to-action {
+                margin-top: 0;
+            }
 
-        .vapor-app__call-to-action
-                background-color: black
-                width: 100%
-                padding: 3em 0
-
-    .application
-        background: white!important
-
-    .wrapper
-        padding: 0 66px
-        max-width: 1500px
-        min-width: 800px
-        margin: 0 auto
-        width: 100%
-    .dark-old
-        color: white
-        padding-top: 64px
-        background: black
-        padding-bottom: 64px
-        min-width: 100%
-        a
-            text-decoration: none
-            color: white
-            border-bottom: 1px dotted rgba(255, 255, 255, 0.5)
-            &:hover
-                border-bottom: 1px dotted rgba(255, 255, 255, 0.75)
-    .bios
-        display: flex
-        .bio
-            width: 33%
-
-
-    .mid-title
-        text-align: center
-        margin-top: 64px
-        margin-bottom: 64px
-        padding: 0
-        font-size: 36px
-
-    .packages
-        padding: 0
-        .package-list
-            display: flex
-            flex-wrap: wrap
-            justify-content: space-around
-            .package
-                flex-basis: 33%
-                text-align: center
-                margin-bottom: 36px
-                display: block    
-
-    .buttons
-        margin: 0 18px
-        padding: 0
-        margin-top: 36px
-        text-align: center
-
-    .contribution-methods
-        display: flex
-        padding-bottom: 64px
-        div
-            width: 33%
-            text-align: center
-            padding: 0 32px
-        img
-            height: 66px
-        p
-            margin-top: 16px
-            margin-bottom: 16px
-            font-size: 18px
-
-    .sponsor-list
-        overflow: hidden
-        display: flex
-        padding-bottom: 64px
-        div
-            width: 33%
-            text-align: center
-            padding: 0 32px
-
-            a
-                color: inherit
-                text-decoration: none
-
-            img
-                width: 112px
-                height: 112px
-
-            p
-                font-size: 18px
-
-    .vapor-footer
-        text-align: center
-        color: #9B9B9B
-        .footer-nav
-            display: block
-            margin-bottom: 8px
-            a
-                font-size: 18px
-                display: inline-block
-                color: inherit
-                text-decoration: none
-                &:hover
-                    color: white
-                    &::after
-                        color: #9B9B9B
-                &:last-child
-                    &::after
-                        display: none
-                &::after
-                    content: '•'
-                    margin-left: 8px
-                    margin-right: 4px
-        padding: 24px 0px
-        background: black
-
-        p
-            color: #4A4A4A
-            margin: 0
-            padding: 0
+            .vapor-app__call-to-action {
+                background-color: black;
+                width: 100%;
+                padding: 3em 0;
+            }
+        }
+    }
 </style>
 
 <script>
@@ -182,6 +58,7 @@
     import VaporCallToAction from 'components/common/callToAction';
     import VaporContribute from 'components/contribute';
     import VaporSponsor from 'components/sponsor';
+    import VaporFooter from 'components/footer';
 
     export default {
         name: 'vaporApp',
@@ -193,7 +70,8 @@
             [VaporPackages.name]: VaporPackages,
             [VaporCallToAction.name]: VaporCallToAction,
             [VaporContribute.name]: VaporContribute,
-            [VaporSponsor.name]: VaporSponsor
+            [VaporSponsor.name]: VaporSponsor,
+            [VaporFooter.name]: VaporFooter
         }
     }
 </script>
---
+++ src/vapor/footer.vue
@@ -0,0 +1,65 @@
+<template>
+    <footer class="vapor-footer">
+        <div class="vapor-footer__link-container">
+            <a class="vapor-footer__link"
+               v-for="link in links"
+               :target="link.target"
+               :href="link.url">{{ link.text }}</a>
+        </div>
+
+        <small class="vapor-footer__copyright">&copy; Qutheory, LLC 2018</small>
+    </footer>
+</template>
+
+<style lang="scss">
+    @import 'src/variables';
+
+    .vapor-footer {
+        color: $color-gray;
+        background-color: black;
+        width: 100%;
+        padding: 2em 0;
+        text-align: center;
+
+        .vapor-footer__link {
+            color: inherit;
+            font-size: larger;
+            text-decoration: none;
+
+            &:hover {
+                color: white;
+            }
+        }
+
+        .vapor-footer__link + .vapor-footer__link {
+            &::before {
+                color: $color-gray !important;
+                content: '\2022';
+                margin: 0 .25em;
+            }
+        }
+
+        .vapor-footer__copyright {
+            display: block;
+            color: $color-dark-gray;
+            margin-top: 1em;
+        }
+    }
+</style>
+
+<script>
+    export default {
+        name: 'vaporFooter',
+        data() {
+            return {
+                links: [
+                    { text: 'Contact Us', url: 'mailto:hello@vapor.codes' },
+                    { text: 'Twitter', url: 'https://twitter.com/codevapor', target: '_blank' },
+                    { text: 'Medium', url: 'https://medium.com/@codevapor', target: '_blank' },
+                    { text: 'Press Kit', url: '/vapor-presskit-2018-05-04.zip' },
+                    { text: 'Jobs', url: 'mailto:jobs@vapor.codes' }
+                ]
+            }
+        }
+    };
+</script>
