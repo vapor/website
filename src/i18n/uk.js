1b1de85d167f25c0e7fe86be7db5c315bd232470
Petro Rovenskyy <petro.rovenskyy@gmail.com>
2019-02-27 16:27:16 +0200

    added ukrainian localization. (#31)

---
+++ src/i18n/uk.js
@@ -0,0 +1,43 @@
+export default {
+    header: {
+        slogan: 'Майбутнє веб розробки.'
+    },
+    callToAction: {
+        start: 'Розпочати',
+        join: 'Чат'
+    },
+    nav: {
+        code: 'Код',
+        cloud: 'Хмара',
+        docs: 'Документація'
+    },
+    bio: {
+        first: 'Неблокуюча, подіє-орієнтована архітектура, побудована на основі фреймворка <a target="_blank" href="https://github.com/apple/swift-nio">SwiftNIO</a> від Apple, забезпечує <a target="_blank" href="https://medium.com/@codevapor/vapor-3-0-0-released-8356fa619a5d">високу продуктивність</a>.',
+        second: 'Написаний за допомогою <a target="_blank" href="https://swift.org">Swift</a>, потужній та сучасній мові програмування, яку легко освоїти.',
+        third: 'Виразний, <a target="_blank" href="https://developer.apple.com/videos/play/wwdc2015/408/">протокол-орієнтований дизайн</a> з акцентом на безпеку типів і якість підтримки коду.'
+    },
+    contribute: {
+        title: 'Зроби свій внесок!',
+        options: {
+            code: {
+                description: 'Пропонуй ідеї, логуй баги або ж створюй пул ріквести в GitHub!'
+            },
+            chat: {
+                title: 'Приєднайся до чату',
+                description: 'Допоможи новачкам освоїти Vapor.'
+            },
+            donate: {
+                title: 'Стати спонсором',
+                description: 'Зроби одноразовий внесок або ж стань постійним спонсором, щоб підтримати проект'
+            }
+        }
+    },
+    sponsors: {
+        title: 'Наші спонсори'
+    },
+    footer: {
+        contact: 'Зв\'язатись з нами',
+        press: 'Прес-кіт',
+        jobs: 'Робота'
+    }
+};
