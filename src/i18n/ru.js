6ef218a1f708a0dff1ecb6531ab64690b3efa13a
Sergey Armodin <dolph-in@yandex.ru>
2019-03-02 04:47:36 +0300

    Russian localization added (#36)
    * Russian localization added
    
    * Russian localization added

---
+++ src/i18n/ru.js
@@ -0,0 +1,43 @@
+export default {
+    header: {
+        slogan: 'Будущее веб-разработки.'
+    },
+    callToAction: {
+        start: 'Начало работы',
+        join: 'Присоединяйтесь к чату'
+    },
+    nav: {
+        code: 'Код',
+        cloud: 'Cloud',
+        docs: 'Документация'
+    },
+    bio: {
+        first: 'Неблокирующая, основанная на событиях архитектура, построенная на <a target="_blank" href="https://github.com/apple/swift-nio">SwiftNIO</a> от Apple с <a target="_blank" href="https://medium.com/@codevapor/vapor-3-0-0-released-8356fa619a5d">высокой производительностью</a>.',
+        second: 'Написано на <a target="_blank" href="https://swift.org">Swift</a> -  мощном языке программирования, который к тому же легко изучить.',
+        third: 'Выразительный, <a target="_blank" href="https://developer.apple.com/videos/play/wwdc2015/408/">протоколо-ориентированный дизайн</a> с фокусом на безопасность типов и поддерживаемость.'
+    },
+    contribute: {
+        title: 'Вносите свой вклад!',
+        options: {
+            code: {
+                description: 'Отправляйте идеи, сообщайте об ошибках и добавляйте код на GitHub!'
+            },
+            chat: {
+                title: 'Присоединяйтесь к чату',
+                description: 'Помогите новичкам освоить Vapor.'
+            },
+            donate: {
+                title: 'Станьте спонсором',
+                description: 'Сделайте единоразовое или периодические пожертвования, чтобы поддержать разработку.'
+            }
+        }
+    },
+    sponsors: {
+        title: 'Наши спонсоры'
+    },
+    footer: {
+        contact: 'Обратная связь',
+        press: 'Пресс-кит',
+        jobs: 'Работа'
+    }
+};
