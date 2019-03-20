9a9cb06382a951f66907e2ad5d9baa552e2920dc
Heidi Puk Hermann <32973281+heidipuk@users.noreply.github.com>
2019-03-20 22:57:27 +0100

    Add Danish localization (#40)
    * Add Danish localization
    
    * Fix spelling errors
    
    * Implement feedback

---
+++ src/i18n/dk.js
@@ -0,0 +1,43 @@
+export default {
+    header: {
+        slogan: 'Fremtidens webudvikling'
+    },
+    callToAction: {
+        start: 'Kom igang',
+        join: 'Join Chat'
+    },
+    nav: {
+        code: 'Kode',
+        cloud: 'Cloud',
+        docs: 'Dokumentation'
+    },
+    bio: {
+        first: 'Ikke-blokerende, event-dreven arkitektur bygget oven på Apple\'s <a target="_blank" href="https://github.com/apple/swift-nio">SwiftNIO</a> leverer Vapor <a target="_blank" href="https://medium.com/@codevapor/vapor-3-0-0-released-8356fa619a5d">høj ydeevne</a>.',
+        second: 'Skrevet i <a target="_blank" href="https://swift.org">Swift</a>, det kraftfulde programmeringssprog, der samtidig er nemt at lære.',
+        third: 'Ekspressivt, <a target="_blank" href="https://developer.apple.com/videos/play/wwdc2015/408/">protokol-orienteret design,</a> med fokus på typesikkerhed og vedligeholdelse.'
+    },
+    contribute: {
+        title: 'Bidrag!',
+        options: {
+            code: {
+                description: 'Deltag i udviklingen, foreslå nye features eller reporter fejl på Github!'
+            },
+            chat: {
+                title: 'Join Chat',
+                description: 'Hjælp nytilkommere lære og komme igang med Vapor.'
+            },
+            donate: {
+                title: 'Bliv sponsor',
+                description: 'lav en engangsdonation eller gentagende donationer for at støtte udviklingen.'
+            }
+        }
+    },
+    sponsors: {
+        title: 'Vores sponsorer'
+    },
+    footer: {
+        contact: 'Kontakt os',
+        press: 'Presse',
+        jobs: 'Jobs'
+    }
+};
